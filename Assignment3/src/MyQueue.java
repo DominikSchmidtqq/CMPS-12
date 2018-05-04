157a2fa2c420dba53a590540cf0c5718f64bf711
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-05-04 02:37:03 -0700

    implemented queue
    /*

--- Assignment3/src/MyQueue.java
+++ Assignment3/src/MyQueue.java
@@ -2,8 +2,56 @@ public class MyQueue implements QueueInterface {
 	/* 
 	* TODO 2: Implement "MyQueue"
 	*/
+	private MyLinkedList list = new MyLinkedList();
 
-	
+	//constructor, creates a new instance of MyQueue
+	public MyQueue() {
+	    list = new MyLinkedList();
+    }
+
+    //checks if the queue contains anything, is the list is empty returns true, otherwise return false
+    public boolean isEmpty() {
+        if (list.size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //adds an object at the last index of the internal list of the queue
+    public void enqueue(Object item) {
+	    if (this.isEmpty()) {
+	        list.add(0, item);
+        } else {
+	        list.add(list.size - 1, item);
+        }
+    }
+
+    //returns and removes the object at the first index of the internal list of the queue, if the queue is empty, throws an exception
+    public Object dequeue() {
+        if (this.isEmpty()) {
+            throw new StackException("Stack is empty");
+        } else {
+            Object o = list.get(0);
+            list.remove(0);
+            return o;
+        }
+    }
+
+    //removes all items from the internal list of the queue by creating a new internal list
+    public void dequeueAll() {
+	    list = new MyLinkedList();
+    }
+
+    //return the item at the first index of the internal list if it exists, other throws an exception
+    public Object peek() {
+	    if (this.isEmpty()) {
+            throw new StackException("Stack is empty");
+	    }
+        else {
+	        return list.get(0);
+        }
+    }
 	/* 
 	* TODO 2: Implement "MyQueue"
 	*/
--- Assignment3/src/MyStack.java
+++ Assignment3/src/MyStack.java
@@ -4,10 +4,12 @@ public class MyStack implements StackInterface {
 	*/
 	private MyLinkedList list;
 
+	// Constructor, creates new Stack instance
     public MyStack() {
         list = new MyLinkedList();
     }
 
+    //checks whether or not the stack is empty
     public boolean isEmpty() {
         if (list.size == 0) {
             return true;
@@ -16,20 +18,32 @@ public class MyStack implements StackInterface {
         }
     }
 
+    //adds an object to the queue, inserting it at the front of the list
     public void push(Object o) {
         list.add(0, o);
     }
 
+    //if an object exists, return it and remove it from the list, otherwise throws an exception
     public Object pop() {
-        Object o = list.get(0);
-        list.remove(0);
-        return o;
+        if (this.isEmpty()) {
+            throw new StackException("Stack is empty");
+        } else {
+            Object o = list.get(0);
+            list.remove(0);
+            return o;
+        }
     }
 
+    //returns the object at the first index in the list without removing it, should it exist, otherwise throws and exception
     public Object peek() {
-        return list.get(0);
+        if (this.isEmpty()) {
+            throw new StackException("Stack is empty");
+        } else {
+            return list.get(0);
+        }
     }
 
+    //pops all objects by creating a new list
     public void popAll() {
         list = new MyLinkedList();
     }
