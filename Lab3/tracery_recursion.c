0e1f1ab6c07de2bab3fe2c2c27747adcaaa7f3b2
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-05-09 01:42:54 -0700

    update
    /*

--- Lab3/tracery_recursion.c
+++ Lab3/tracery_recursion.c
@@ -3,12 +3,21 @@
 
 char* make_string_from(char* from, int count) {
 	/* TODO 2 */
+    char* array = malloc(count + 1);
+    for (int i = 0; i < count; i++) {
+        array[i] = from[i];
+    }
+
+    return  array;
 	return NULL;
 	/* TODO 2 */
 }
 
 int main(int argc, char** argv) {
 	/* TODO 1 */
+    for (int i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
  	/* TODO 1 */
 
   
