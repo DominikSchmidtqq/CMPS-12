c44d5d905bcc210c8bd8f24bbb51b9a483bccd50
DominikSchmidtqq <dominikschmidt1893@gmail.com>
2018-05-13 16:46:52 -0700

    update todo 3
    /*

--- Lab3/tracery_recursion.c
+++ Lab3/tracery_recursion.c
@@ -23,18 +23,32 @@ int main(int argc, char** argv) {
     }
  	/* TODO 1 */
 
-    char char_buffer[64];
+    char char_buffer[1000];
     int buffer_index;
     char* rule;
     char* expansion;
 
     while(getchar() != EOF) {
-        if(getchar() == ":") {
+        if (getchar() == ":") {
             rule = make_string_from(char_buffer, buffer_index);
             buffer_index = 0;
+
+            if(rule != NULL) {
+                free(rule);
+                rule = NULL;
+            }
         } else if ((getchar() == ",") || (getchar() == "/n")) {
             expansion = make_string_from(char_buffer, buffer_index);
             buffer_index = 0;
+            printf("A potential expansion of rule '%s' is '%s'\n", rule, expansion);
+
+            if (expansion != NULL) {
+                free(expansion);
+                expansion = NULL;
+            }
+        } else {
+            char_buffer[buffer_index] = getchar();
+            buffer_index++;
         }
 
     }
